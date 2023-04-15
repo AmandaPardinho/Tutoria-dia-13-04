d16a72765d28ca6777e901b20a0b760a0326d962
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-15 17:25:57 -0300

    Função para deletar foi criada

--- crud_simulacao.py
+++ crud_simulacao.py
@@ -9,12 +9,18 @@ produtos = {
     "preco": ""
 }
 
+# Simula a criação do id pelo banco de dados
 def gerar_id():
     id = len(produtos) + 1
     return id
 
+# Cria um novo produto no dicionário
 @app.route("/produtos", methods=['POST'])
 def criar_produtos(nome, descricao, preco):
     produtos[gerar_id()] = {"nome": nome, "descricao": descricao, "preco": preco}
+
+@app.route("/produtos", methods=['POST'])  
+def deleta_produto(id:int):
+    del produtos[id]
 
 app.run(debug=True) 
