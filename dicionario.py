aab50c23987bacc968f82d2074109821383d2782
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-20 16:56:31 -0300

    Criação de dicionário

---
+++ dicionario.py
@@ -0,0 +1,20 @@
+produtos = {
+    1: {
+        "nome": "Lapiseira Pentel Técnica Vermelha",
+        "descricao": "Lapiseira para desenho técnico com grafite de 0.3mm",
+        "marca": "Pentel",
+        "preco": 18.99
+    },
+    2: {
+        "nome": "Post It 3M",
+        "descricao": "Post It 3M Pink",
+        "marca": "3M",
+        "preco": 9.99
+    },
+    3: {
+        "nome": "Patinho de borracha",
+        "descricao": "Patinho de borracha amarelo",
+        "marca": "Suzano",
+        "preco": 5.99
+    }
+}
