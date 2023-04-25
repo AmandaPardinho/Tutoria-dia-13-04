1e2548451120152bdacdacde94ac6271bb437f10
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-25 20:38:03 -0300

    Criação da rota para retornar todos os produtos

--- api/app.py
+++ api/app.py
@@ -3,8 +3,17 @@ import dictionary
 
 app = Flask(__name__)
 
-@app.route("/produtos/<int:id>", methods = ['GET'])
+# Route to return a single product by id
+@app.route("/products/<int:id>", methods = ['GET'])
 def show_product(id:int):
     return dictionary.return_product(id)
+
+# Route to return all products
+@app.route("/products", methods = ['GET'])
+def show_all():
+    return dictionary.return_products()
+
+#
+
 
 app.run(debug=True)
