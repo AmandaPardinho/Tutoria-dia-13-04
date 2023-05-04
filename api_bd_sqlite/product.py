590cb9fc8401dd082f97251e6715740126a7892f
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-05-04 12:44:07 -0300

    Criação do método para deletar produto e correção do método para atualizar produto

--- api_bd_sqlite/product.py
+++ api_bd_sqlite/product.py
@@ -3,7 +3,6 @@ import os, sqlite3
 way = os.path.dirname(os.path.realpath(__file__))
 db_path_way = os.path.join(way, 'products.db')
 
-
 class Products:
     def __init__(self, name: str, description: str, weight: float , brand: str, supplier: str, price: float,  id = None):
         self.id = id
@@ -39,13 +38,12 @@ class Products:
         query = "SELECT * FROM products WHERE id_product = ?"
         cursor = conn.cursor()
         answer = cursor.execute(query, (id, )).fetchone()
+        conn.close()
         if answer:
             return Products(id = answer[0], name = answer[1], description = answer[2], weight = answer[3], brand = answer[4], supplier = answer[5], price = answer[6])
         else: 
             return None
 
-
-    
     def create_product(self, conn: sqlite3.Connection):
         if self.id is None:
             query = "INSERT INTO products (name_product, description_product, weight_product, brand_product, supplier_product, price_product) VALUES (?, ?, ?, ?, ?, ?)"
@@ -53,10 +51,15 @@ class Products:
             cursor.execute(query, (self.name, self.description, self.weight, self.brand, self.supplier, self.price))
             self.id = cursor.lastrowid
         else:
-            query = "UPDATE products SET (name_product = ?, description_product = ?, weight_product = ?, brand_product = ?, supplier_product = ?, price_product = ?) WHERE id_product = ?"
-            conn.execute(query, (self.id, ))
+            query = "UPDATE products SET name_product = ?, description_product = ?, weight_product = ?, brand_product = ?, supplier_product = ?, price_product = ? WHERE id_product = ?"
+            cursor = conn.cursor()
+            cursor.execute(query, (self.name, self.description, self.weight, self.brand, self.supplier, self.price, self.id))
         conn.commit()
         conn.close()
 
-
-
+    def delete_product(self, conn: sqlite3.Connection):
+        query = "DELETE FROM products WHERE id_product = ?"
+        cursor = conn.cursor()
+        cursor.execute(query, (self.id, ))
+        conn.commit()
+        conn.close()
